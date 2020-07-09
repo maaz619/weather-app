ca79c4cd6a83f5fdaa6e898fc159147eacb422c0
maaz619 <ahmedmaaz919@gmail.com>
2020-07-09 14:49:44 +0530

    working through API

--- src/Components/WeatherBox.jsx
+++ src/Components/WeatherBox.jsx
@@ -1,58 +1,103 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Weather.css";
+import Axios from "axios";
 
-const WeatherBox = () => {
-  const temp = 10 + "c";
-  const location = "Koderma";
-  const type = "Cloudy";
-  const forcast = [10, 20, 30, 40, 50, 60];
-  const details = [
-    { title: "Temperature", value: 29 + "c" },
-    { title: "Condition", value: "Sunny" },
-    { title: "Wind", value: "6 km/h" },
-    { title: "Humidity", value: "90%" },
-  ];
-  return (
-    <div className="container">
-      <div className="weatherbox">
-        <div className="temp-loc">
-          <div className="location-box">
-            <h1>{location}</h1>
-          </div>
-          <div className="temperature-box">{temp}</div>
-        </div>
-        <div className="weather-type">
-          <div className="type">
-            <h1>{type}</h1>
-          </div>
-        </div>
-        <div className="today-forcast">
-          {forcast.map((tempa, j) => (
-            <div className="all-forcast" key={j}>
-              {tempa + "c"}
+class WeatherBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: "",
+      location: [],
+      type: "",
+      forcast: [10, 20, 30, 40, 50, 60],
+      details: [
+        { title: "Temperature", value: "" },
+        { title: "Condition", value: "" },
+        { title: "Wind", value: "" },
+        { title: "Humidity", value: "" },
+      ],
+    };
+  }
+  async componentDidMount() {
+    const currentWeather = await Axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?id=1266414&appid=005979b9efbaa27908bf6de270165897"
+    );
+    this.thedata = currentWeather.data;
+    this.setState({
+      temp: this.thedata.main.temp,
+      loaction: this.thedata.name,
+      type: this.thedata.weather.main,
+      details: [
+        { title: "Temperature", value: this.thedata.main.temp },
+        { title: "Condition", value: [this.thedata.weather.main] },
+        { title: "Wind", value: this.thedata.wind.speed },
+        { title: "Humidity", value: this.thedata.main.humidity },
+      ],
+    });
+  }
+  handleClick = async () => {
+    const currentWeather = await Axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?id=1266414&appid=005979b9efbaa27908bf6de270165897"
+    );
+    this.thedata = currentWeather.data;
+    this.setState({
+      temp: this.thedata.main.temp,
+      loaction: this.thedata["name"],
+      type: this.thedata.weather.main,
+      details: [
+        { title: "Temperature", value: this.thedata.main.temp },
+        { title: "Condition", value: this.thedata.weather["main"] },
+        { title: "Wind", value: this.thedata.wind.speed },
+        { title: "Humidity", value: this.thedata.main.humidity },
+      ],
+    });
+    console.log("clicked");
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="weatherbox">
+          <div className="temp-loc">
+            <div className="location-box">
+              <h1>{this.state.location}</h1>
             </div>
-          ))}
-        </div>
-        <div className="weather-details">
-          {details.map((detail, i) => (
-            <div className="ti-va" key={i}>
-              <div className="title" key={i}>
-                {detail.title}
-              </div>
-              <div className="value" key={i}>
-                {detail.value}
-              </div>
+            <div className="temperature-box">{this.state.temp}</div>
+          </div>
+          <div className="weather-type">
+            <div className="type">
+              <h1>{this.state.type}</h1>
             </div>
-          ))}
-        </div>
-        <div className="update-button">
-          <div>
-            <button className="btn btn-primary"> refresh</button>
+          </div>
+          <div className="today-forcast">
+            {this.state.forcast.map((temperature, j) => (
+              <div className="all-forcast" key={j}>
+                {temperature + "c"}
+              </div>
+            ))}
+          </div>
+          <div className="weather-details">
+            {this.state.details.map((detail, i) => (
+              <div className="ti-va" key={i}>
+                <div className="title" key={i}>
+                  {detail.title}
+                </div>
+                <div className="value" key={i}>
+                  {detail.value}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="update-button">
+            <div>
+              <button className="btn btn-primary" onClick={this.handleClick}>
+                refresh
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default WeatherBox;
